e1279c6cd8a1a7629461bf869076e9d0f22e8b72
John Zhu <zxs.dev@outlook.com>
2020-12-03 15:00:23 +0800

    new component lab/unsubscribe

--- src/app/lab/lab.module.ts
+++ src/app/lab/lab.module.ts
@@ -3,18 +3,22 @@ import { CommonModule } from '@angular/common';
 import { WebworkerComponent } from './webworker/webworker.component';
 import { Routes, RouterModule } from '@angular/router';
 import { SitemapComponent } from './sitemap/sitemap.component';
+import { UnsubscribeComponent } from './unsubscribe/unsubscribe.component';
+import { HttpClientModule } from '@angular/common/http';
 
 const labRoutes: Routes = [
   { path: 'webworker', component: WebworkerComponent },
   { path: 'sitemap', component: SitemapComponent },
-  { path: '', redirectTo: 'sitemap', pathMatch: 'full' }
+  { path: 'unsubscribe', component: UnsubscribeComponent },
+  { path: '', redirectTo: 'unsubscribe', pathMatch: 'full' }
 ];
 
 @NgModule({
-  declarations: [WebworkerComponent, SitemapComponent],
+  declarations: [WebworkerComponent, SitemapComponent, UnsubscribeComponent],
   imports: [
     CommonModule,
-    RouterModule.forChild(labRoutes)
+    RouterModule.forChild(labRoutes),
+    HttpClientModule
   ]
 })
 export class LabModule { }
---
+++ src/app/lab/unsubscribe/unsubscribe.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-unsubscribe',
+  templateUrl: './unsubscribe.component.html',
+  styleUrls: ['./unsubscribe.component.css']
+})
+export class UnsubscribeComponent implements OnInit {
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.abortControllerTest();
+    // this.rxjsTest();
+  }
+  private abortControllerTest(): void {
+    try {
+      // abort in 1 ms
+      const controller = new AbortController();
+      const response = fetch('https://fonts.googleapis.com/icon?family=Material+Icons', {
+        signal: controller.signal,
+      });
+      response.then((result) => {
+        console.log('promise.then:onfulfilled');
+      }, (error) => {
+        console.log('promise.then:onrejected');
+      });
+      response.catch((error) => {
+        console.log('promise.catch');
+      });
+      response.finally(() => {
+        console.log('promise.finally');
+      });
+      // cancel immediately
+      controller.abort();
+    } catch (err) {
+      if (err.name === 'AbortError') { // handle abort()
+        console.log('❤');
+      } else {
+        throw err;
+      }
+    }
+  }
+  /** test case for rxjs */
+  private rxjsTest(): void {
+    const observer = this.http.get('https://fonts.googleapis.com/icon?family=Material+Icons');
+    const sub = observer.subscribe((result) => {
+      console.log(`🙂`);
+      console.log(result);
+    }, error => {
+      console.log(`😫`);
+      console.error(error);
+    }, () => {
+      console.log(`complete`);
+    });
+
+    // cancel immediately
+    sub.unsubscribe();
+  }
+}
