aede058496a01227efce8966ae113679588ae4ee
ZXS66 <zxs.dev@outlook.com>
2024-12-11 22:18:34 +0800

    fix broken links

--- src/app/lab/emoji-ww.worker.ts
+++ src/app/lab/emoji-ww.worker.ts
@@ -42,7 +42,8 @@ const loadEmojiMetadata = async () => {
     // loaded
     return;
   }
-  const fileLink = 'https://zxs66.github.io/images/emoji-knowledge-review/full-emoji-list.json';
+  // const fileLink = 'https://zxs66.github.io/images/emoji-knowledge-review/full-emoji-list.json';
+  const fileLink = `https://johnzhu.online/blog/images/emoji-knowledge-review/full-emoji-list.json`;
   const handleError = (err) => {
     console.error(err);
     EMOJIMETADATA_CACHE = new Set();
