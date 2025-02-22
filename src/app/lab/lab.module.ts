f6fd1bbfa581e4acb2072c0c90c8f527071104a7
ZXS66 <zxs.dev@outlook.com>
2025-02-22 12:50:02 +0800

    update ng from 17 to 18

--- src/app/lab/lab.module.ts
+++ src/app/lab/lab.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { UnsubscribeComponent } from './unsubscribe/unsubscribe.component';
 import { EmojiComponent } from './emoji/emoji.component';
 import { SubMaterialModule } from '../sub-material/sub-material.module';
@@ -12,13 +12,7 @@ const labRoutes: Routes = [
   { path: '', redirectTo: 'unsubscribe', pathMatch: 'full' }
 ];
 
-@NgModule({
-  declarations: [UnsubscribeComponent, EmojiComponent],
-  imports: [
-    CommonModule,
-    RouterModule.forChild(labRoutes),
-    HttpClientModule,
-    SubMaterialModule
-  ]
-})
+@NgModule({ declarations: [UnsubscribeComponent, EmojiComponent], imports: [CommonModule,
+        RouterModule.forChild(labRoutes),
+        SubMaterialModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class LabModule { }
