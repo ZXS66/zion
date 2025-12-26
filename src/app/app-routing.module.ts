19266472c8daf2f7add563e119bd3579794fde02
ZXS <zxs.dev@outlook.com>
2025-12-26 22:48:15 +0800

    remove resume module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,6 @@ import { SitemapComponent } from "./sitemap/sitemap.component";
 import { UnknownComponent } from "./unknown/unknown.component";
 
 const routes: Routes = [
-  // {
-  //   path: "resume",
-  //   loadChildren: () =>
-  //     import("./resume/resume.module").then((_) => _.ResumeModule),
-  // },
   {
     path: "lab",
     loadChildren: () => import("./lab/lab.module").then((_) => _.LabModule),
